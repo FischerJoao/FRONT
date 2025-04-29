d5df442861a06422d90694445507d77468a05f01
FischerJoao <joao-fischer@hotmail.com>
2025-04-28 21:52:09 -0300

    Refactor Delete

--- components/Delete.js
+++ components/Delete.js
@@ -1,16 +1,12 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Button } from "react-native-paper";
+const Delete = (id) => {
+    let url = `http://localhost:3000/delete/${id}`;
+    console.log(url);
+    fetch(url, {
+        method: 'DELETE',
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .catch((error) => console.error("Erro ao deletar:", error));
+};
 
-const DadosDeletado = (props) => {
-    const Delete = (id) => {
-        let url = `http://localhost:3000/delete/${id}`;
-        console.log(url);
-        fetch(url, {
-            method: 'DELETE',
-        }).then((response) => response.json())
-            .then((json) => console.log(json));
-    };
-}
-
-export default DadosDeletado;
+export default Delete;
